5b8c02ae4786e88f72e750686e17b6356922357f
AristideI <a.isingizwe@alustudent.com>
2023-10-03 09:00:24 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,69 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import Dice from "./components/Dice";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 function App() {
-  let [start, setStart] = useState(1);
-  let [arr, setArr] = useState([]);
-  let [goal, setGoal] = useState("None");
-  useEffect(() => {
-    let newArr = [...arr];
-    for (let i = 0; i < 10; i++) {
-      newArr.push(Math.floor(Math.random() * 9));
-    }
-    setArr(newArr);
-  }, [start]);
+  let [arr, setArr] = useState(newDice());
+  let [goal, setGoal] = useState(false);
 
-  function goals(elt) {
-    setGoal(elt);
+  console.log(arr);
+
+  function generate() {
+    return {
+      value: Math.ceil(Math.random() * 9),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+  function newDice() {
+    const newarr = [];
+    for (let i = 0; i < 10; i++) {
+      newarr.push(generate());
+    }
+
+    return newarr;
   }
 
   function handleRoll() {
-    setArr([
-      ...arr.map((elt) => {
-        if (elt != goal) {
-          return Math.floor(Math.random() * 9);
+    if (goal) {
+      setArr(newDice());
+      setGoal(false);
+    } else {
+      setArr((arr) => {
+        return arr.map((elt) => {
+          if (elt.isHeld) {
+            return elt;
+          } else {
+            return generate();
+          }
+        });
+      });
+    }
+  }
+
+  function HoldDice(id) {
+    setArr((arr) => {
+      return arr.map((elt) => {
+        if (elt.id === id) {
+          return { ...elt, isHeld: !elt.isHeld };
         } else {
           return elt;
         }
-      }),
-    ]);
+      });
+    });
   }
+
+  useEffect(() => {
+    const pro = arr[0].value;
+    if (arr.every((elt) => elt.isHeld && elt.value == pro)) {
+      setGoal(true);
+    }
+  }, [arr]);
 
   return (
     <article className="flex flex-col justify-center items-center h-screen gap-16">
+      {goal && <Confetti />}
       <section>
         <p className="font-bold font-serif text-5xl text-center">
           Tenzies Game
@@ -43,14 +75,12 @@ function App() {
         </p>
       </section>
       <section className="w-2/5 grid grid-cols-5 gap-6">
-        {arr.map((elt, ind) => (
+        {arr.map((elt) => (
           <Dice
-            val={elt}
-            checked={false}
-            key={nanoid()}
-            id={ind}
-            goal={goal}
-            setgoal={goals}
+            key={elt.id}
+            value={elt.value}
+            isHeld={elt.isHeld}
+            hold={() => HoldDice(elt.id)}
           />
         ))}
       </section>
@@ -58,7 +88,7 @@ function App() {
         onClick={handleRoll}
         className="font-semibold text-2xl border-2 border-violet-400 bg-violet-300 px-12 py-2 rounded-xl"
       >
-        Roll
+        {goal ? "New Game" : "Roll"}
       </button>
     </article>
   );
--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,30 +1,19 @@
 import { useState } from "react";
 export default function Dice(props) {
-  let [check, setCheck] = useState(props.checker);
-
   let styles = {
-    backgroundColor: check ? "#a78bfa" : "#ede9fe",
-    border: check ? "2px solid #7c3aed" : "2px solid #a78bfa",
+    backgroundColor: props.isHeld ? "#a78bfa" : "#ede9fe",
+    border: props.isHeld ? "2px solid #7c3aed" : "2px solid #a78bfa",
   };
 
-  function handleClick(e) {
-    if (props.goal === "None") {
-      props.setgoal(e.target.value);
-    } else {
-      if (e.target.value === props.goal) {
-        setCheck(!check);
-      }
-    }
-  }
   return (
     <button
       style={styles}
-      onClick={handleClick}
       id={props.id}
       value={props.val}
+      onClick={props.hold}
       className="h-20 font-semibold text-3xl rounded-xl shadow-xl shadow-violet-300 font-serif grid place-content-center"
     >
-      {props.val}
+      {props.value}
     </button>
   );
 }
